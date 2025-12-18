68f9889df2b79ef9585bb961d34c702f2d8cf90b
Isaa1126 <isalkaino@gmail.com>
2025-12-18 16:40:38 +0530

    Fix app entry point and screen imports

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'screens/login_page.dart';
+
+void main() {
+  runApp(const HBTSApp());
+}
+
+class HBTSApp extends StatelessWidget {
+  const HBTSApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'HBTS',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
