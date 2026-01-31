c0b3f5fbc0a68b892c55ee8b7e4af48e22b1f060
isalka25 <pulasthi.uog04@edu.lnbti.lk>
2026-01-31 16:11:41 +0530

    added the closest booking to the current time in a card to home page

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -16,6 +16,7 @@ import 'screens/confirm_booking_page.dart';
 import 'screens/booking_success_page.dart';
 
 import 'admin/dashboard.dart';
+import 'screens/upcoming_today_page.dart';
 
 
 class AppRoutes {
@@ -27,6 +28,7 @@ class AppRoutes {
   static const trackMyBooking = '/track-my-booking';
   static const trackBus = '/track-bus';
   static const notifications = '/notifications';
+  static const upcomingToday = '/upcoming-today';
 
 
   static const tripDetails = '/trip-details';
@@ -68,6 +70,10 @@ class AppRoutes {
 
       case trackBus:
         return MaterialPageRoute(builder: (_) => const TrackBusPage());
+
+
+      case upcomingToday: 
+      return MaterialPageRoute(builder: (_) => const UpcomingTodayPage());  
 
       // ✅ NEW FLOW ROUTES (must be BEFORE default)
       case tripDetails:
